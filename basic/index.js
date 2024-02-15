2da7e930f2b8e6fe09c860ce4e746222dae6c40f
Pepito94510 <kevin.boissiere94@gmail.com>
2024-02-15 11:07:32 +0100

    add temperature converter

--- basic/index.js
+++ basic/index.js
@@ -2,4 +2,19 @@ function helloWorld() {
     return 'hello world';
 }
 
-export default helloWorld;
+function convertisseurTemperature(temperature, unite) {
+    switch (unite) {
+        case 'C':
+            temperature = (temperature * 9 / 5) + 32;
+            break;
+        case 'F':
+            temperature = (temperature - 32) * 5 / 9;
+            break;
+        default:
+            temperature = 'Error, unite unknow';
+            break;
+    }
+    return temperature;
+}
+
+export default { helloWorld, convertisseurTemperature };
--- basic/tests/script.js
+++ basic/tests/script.js
@@ -1,3 +1,7 @@
 const helloWorld = require('basic');
 
+const convertisseurTemperature = require('basic');
+
 console.log(helloWorld());
+
+console.log(convertisseurTemperature());
